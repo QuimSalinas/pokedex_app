eec8f26af4084b8c06d54e2d946445b03dd76dc4
QuimSalinas <salinas.volta2@gmail.com>
2020-06-27 15:54:10 +0200

    Updates

---
+++ precache-manifest.18385badfc542721da815a4611a415c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5fecbc51a8f5cf75d6f40d97c2ccd3",
+    "url": "/pokedex_app/index.html"
+  },
+  {
+    "revision": "dbbe3b5aeb8cc34b7aff",
+    "url": "/pokedex_app/static/css/main.25dd995c.chunk.css"
+  },
+  {
+    "revision": "2448bc44a6e7b1af2127",
+    "url": "/pokedex_app/static/js/2.818e2a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex_app/static/js/2.818e2a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbe3b5aeb8cc34b7aff",
+    "url": "/pokedex_app/static/js/main.578a41d2.chunk.js"
+  },
+  {
+    "revision": "0ebe45c780a3dd59ad3b",
+    "url": "/pokedex_app/static/js/runtime-main.2797abd2.js"
+  }
+]);
